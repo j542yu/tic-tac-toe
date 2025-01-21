0b3a5fa2b1b198aa7c1c6a85dfaa2c5c6e412f32
judywsl <j542yu@uwaterloo.ca>
2025-01-21 13:08:49 -0500

    Add check for conditions that end game
    Game will end if board is full (tie) or
    if a player has gotten three in a line

--- lib/game.rb
+++ lib/game.rb
@@ -4,11 +4,41 @@
 class Game
   attr_reader :board, :players
 
+  WINNING_COMBINATIONS = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ].freeze
+
   def initialize
     @board = Array.new(10)
 
     @players = [HumanPlayer.new('one', self, 'X'), HumanPlayer.new('two', self, 'O')]
     puts "#{players[0].name} is facing off #{players[1].name}. Let the game begin!"
+  end
+
+  def board_full?
+    vacant_positions = (1..9).select do |position|
+      board[position].nil?
+    end
+    vacant_positions.empty?
+  end
+
+  def player_has_won?
+    winning_line('X') || winning_line('O')
+  end
+
+  def winning_line(marker)
+    WINNING_COMBINATIONS.each do |combination|
+      markers_in_line = combination.select { |position| board[position] == marker }
+      return true if markers_in_line.length == 3
+    end
+    false
   end
 
   def print_board
