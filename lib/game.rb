8675ea4b031e5bc9fac7351650a529a0ce102893
judywsl <j542yu@uwaterloo.ca>
2025-01-21 14:04:03 -0500

    Implement play game logic
    Keep alternating between human players
    until a tie is reached or someone has won

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,6 @@
 
 # Manages current board and player interactions
 class Game
-  attr_reader :board, :players
-
   WINNING_COMBINATIONS = [
     [1, 2, 3],
     [4, 5, 6],
@@ -18,14 +16,71 @@ class Game
   def initialize
     @board = Array.new(10)
 
+    @current_player_idx = 0
     @players = [HumanPlayer.new('one', self, 'X'), HumanPlayer.new('two', self, 'O')]
-    puts "#{players[0].name} is facing off #{players[1].name}. Let the game begin!"
+
+    puts "\n#{players[0].name} is facing off #{players[1].name}. Let the game begin!"
+    puts "#{current_player} goes first.\n"
+    print_board
+  end
+
+  attr_reader :board, :players
+  attr_accessor :current_player_idx
+
+  def current_player
+    players[current_player_idx]
+  end
+
+  def other_player
+    players[other_player_idx]
+  end
+
+  def play
+    while !board_full? && !player_has_won?
+      place_current_player_marker
+      switch_players
+      print_board
+    end
+    if board_full?
+      puts "\nIt's a tie! Game over."
+    elsif player_has_won?
+      puts "\n#{other_player} has won! Game over."
+    end
+  end
+
+  def place_current_player_marker
+    position = select_player_move
+    board[position] = current_player.marker
+  end
+
+  def select_player_move
+    puts "\n#{current_player}, place your move! Pick a number between 1 and 9"
+    print '=> '
+    position = gets.chomp.to_i
+    until position.between?(1, 9) && vacant_positions.include?(position)
+      puts 'Invalid choice. Please choose another number'
+      print '=> '
+      position = gets.chomp.to_i
+    end
+
+    puts "\n#{current_player} placed their move!"
+
+    position
+  end
+
+  def other_player_idx
+    1 - current_player_idx
+  end
+
+  def switch_players
+    self.current_player_idx = other_player_idx
+  end
+
+  def vacant_positions
+    (1..9).select { |position| board[position].nil? }
   end
 
   def board_full?
-    vacant_positions = (1..9).select do |position|
-      board[position].nil?
-    end
     vacant_positions.empty?
   end
 
@@ -46,7 +101,7 @@ class Game
     row_separator = '--+---+--'
 
     get_position_label = lambda do |position|
-      @board[position] || position.to_s
+      board[position] || position.to_s
     end
 
     row_positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
-require_relative 'lib/game'
+# frozen_string_literal: true
 
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts 'Welcome to Tic Tac Toe!'
 test = Game.new
-test.print_board
+test.play
